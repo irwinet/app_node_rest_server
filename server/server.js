29dbb8d9562265f71bff83b2adee48ce2edd2ecd
Irwin <irwinestradatorres@gmail.com>
2021-01-18 19:08:31 -0500

    Add Body-Parser

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-    res.json('Hello World')
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario');
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    res.json({
+        persona: body
+    });
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
 });
 
 app.listen(3000, () => {
