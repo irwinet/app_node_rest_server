b8d6d1017a996784ec46ac3f316ff0dce0790b4c
Irwin <irwinestradatorres@gmail.com>
2021-03-22 17:45:10 -0500

    Add DotEnv And Cors

---
+++ models/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+var cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        //Middlewares
+        this.middlewares();
+
+        //Rutas de mi aplicacion
+        this.routes();
+    }
+
+    middlewares() {
+        //CORS
+        this.app.use(cors());
+        //Directorio Publico
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.get('/api', (req, res) => {
+            res.json({
+                msg: 'get API'
+            });
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                msg: 'put API'
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.status(201).json({
+                msg: 'post API'
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                msg: 'delete API'
+            });
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                msg: 'patch API'
+            });
+        });
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Escuchando puerto: ', this.port);
+        });
+    }
+
+}
+
+module.exports = Server;
