4f77770170b476a83dd6c437081d6cb4cde7948b
Irwin <irwinestradatorres@gmail.com>
2021-03-24 11:08:13 -0500

    Validar rol con BD

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
+const Role = require('../models/role');
 
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
 
@@ -14,7 +15,13 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe de ser mas de 6 letras').isLength({ min: 6 }),
     check('correo', 'El correo no es valido').isEmail(),
-    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    //check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(async(rol = '') => {
+        const existeRol = await Role.findOne({ rol });
+        if (!existeRol) {
+            throw new Error(`El rol ${ rol } es esta registrado en la BD`);
+        }
+    }),
     validarCampos
 ], usuariosPost);
 
