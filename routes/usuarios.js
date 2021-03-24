286e65ddb5aa66d4d6251ff8d208e98a9b9cbbea
Irwin <irwinestradatorres@gmail.com>
2021-03-24 10:55:55 -0500

    Create Middlewares Show Errors

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
 
 const router = Router();
@@ -9,7 +11,11 @@ router.get('/', usuariosGet);
 router.put('/:id', usuariosPut);
 
 router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de ser mas de 6 letras').isLength({ min: 6 }),
     check('correo', 'El correo no es valido').isEmail(),
+    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos
 ], usuariosPost);
 
 router.delete('/', usuariosDelete);
