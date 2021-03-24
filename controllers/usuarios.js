a4ea76b521ba233050b81b514d14fbfb1158376d
Irwin <irwinestradatorres@gmail.com>
2021-03-24 10:43:16 -0500

    Validate Email

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,6 +2,7 @@ const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
 
 const usuariosGet = (req = request, res = response) => {
     const { q, nombre = "No name", apiKey } = req.query;
@@ -14,12 +15,22 @@ const usuariosGet = (req = request, res = response) => {
 }
 
 const usuariosPost = async(req, res = response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
+    }
+
     const { nombre, correo, password, rol } = req.body;
 
     const usuario = new Usuario({ nombre, correo, password, rol });
 
     //Verrificar si el correo existe
-
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'Ese correo ya esta registrado'
+        });
+    }
     //Encriptar el contraseña
     const salt = bcryptjs.genSaltSync();
     usuario.password = bcryptjs.hashSync(password, salt);
