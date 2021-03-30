6393ad16a0dcfd19722c068e23f83f260eb3af15
Irwin <irwinestradatorres@gmail.com>
2021-03-30 10:27:20 -0500

    Login User

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,44 @@
 const { response } = require("express");
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
-    res.json({
-        msg: 'Login ok'
-    });
+const login = async(req, res = response) => {
+    const { correo, password } = req.body;
+
+    try {
+
+        //Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            });
+        }
+        //Si el usuario esta activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado : false'
+            });
+        }
+        //Verificar la contraseña
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+        //Generar el JWT
+
+        res.json({
+            msg: 'Login ok'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 module.exports = {
