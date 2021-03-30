bf565860ac54757c396f8cadeedf12adf74cedc4
Irwin <irwinestradatorres@gmail.com>
2021-03-30 11:57:15 -0500

    Create Middleware Valid Role

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,6 +5,7 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { esAdminRole } = require('../middlewares/validar-roles');
 
 const router = Router();
 
@@ -29,6 +30,7 @@ router.post('/', [
 
 router.delete('/:id', [
     validarJWT,
+    esAdminRole,
     check('id', 'No es un ID valido').isMongoId(),
     check('id').custom(existeUsuarioPorId),
     validarCampos
