81ae94e7085a02266c31c943df51fa1026801ca0
Irwin <irwinestradatorres@gmail.com>
2021-04-02 15:32:19 -0500

    Create Middleware Validar Archivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -5,13 +5,6 @@ const { Usuario, Producto } = require('../models');
 
 const cargarArchivo = async(req, res = response) => {
 
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-        res.status(400).json({
-            msg: 'No hay archivos que subir'
-        });
-        return;
-    }
-
     try {
         //Imagenes
         //const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
@@ -24,6 +17,7 @@ const cargarArchivo = async(req, res = response) => {
 }
 
 const actualizarImagen = async(req, res = response) => {
+
     const { id, coleccion } = req.params;
 
     let modelo;
