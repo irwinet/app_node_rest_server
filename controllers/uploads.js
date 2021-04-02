27250528ad08910c45668bdd2a477b9150c43a6d
Irwin <irwinestradatorres@gmail.com>
2021-04-02 10:08:16 -0500

    Create Helper Subir Archivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,8 +1,7 @@
 const { response } = require("express");
+const { subirArchivo } = require("../helpers");
 
-const path = require('path');
-
-const cargarArchivo = (req, res = response) => {
+const cargarArchivo = async(req, res = response) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
         res.status(400).json({
@@ -11,18 +10,10 @@ const cargarArchivo = (req, res = response) => {
         return;
     }
 
-    const { archivo } = req.files;
+    //Imagenes
+    const nombre = await subirArchivo(req.files);
 
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
-
-    archivo.mv(uploadPath, (err) => {
-        if (err) {
-            return res.status(500).json({ err });
-        }
-
-        res.json({ msg: 'File uploaded to ' + uploadPath });
-    });
-
+    res.json({ nombre });
 }
 
 module.exports = {
